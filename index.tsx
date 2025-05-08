235033ce4c39b2154f70616efda8d6420ff5fe64
Mishieck <mishieckmwale@gmail.com>
2025-05-08 11:19:43 +0200

    feat(index): handle environment variables and add shebang

--- index.tsx
+++ index.tsx
@@ -1,3 +1,5 @@
+#!/bin/env bun
+
 import { type React, useEffect } from 'react';
 import { render, Box } from 'ink';
 import { TodosUi } from './src/components/todos';
@@ -7,9 +9,15 @@ import { FileSystem } from './src/objects/file-system';
 import { todos } from './src/objects/todos';
 
 const defaultTodoListFile = new URL('./todos.json', import.meta.url).toString();
+const envFilePath = process.env[FileSystem.todoListFilePathEnvVar];
 
-if (!process.env[FileSystem.todoListFilePathEnvVar])
+if (envFilePath) {
+  // TODO: Check to see if this works on Windows
+  if (/[/\\]/.test(envFilePath))
+    process.env[FileSystem.todoListFilePathEnvVar] = `file://${envFilePath}`;
+} else {
   process.env[FileSystem.todoListFilePathEnvVar] = defaultTodoListFile;
+}
 
 
 const App: React.FC = () => {
